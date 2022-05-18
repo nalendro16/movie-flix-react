7b889c572e4e16451d2c9369d9fa39538b8f7d4d
nalendro16 <nalendro_agung@yahoo.com>
2022-05-18 21:43:37 +0700

    feat: add Movie component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './App.css'
 
+import { Movie } from './components'
+
 function App() {
-  return <div className='App'>Movie FLix</div>
+  return (
+    <div className='App'>
+      <Movie />
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/Movie.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Movie = () => {
+  return (
+    <div>
+      <h2>Movie</h2>
+      <div>
+          <img src="" alt="" className='img-poster'/>
+      </div>
+    </div>
+  )
+}

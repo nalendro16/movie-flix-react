0d133cbc9a60ea7839a5d90cb05ae980a73646c9
nalendro16 <nalendro_agung@yahoo.com>
2022-05-18 21:47:21 +0700

    refacto: add more props to each components

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ function App() {
   return (
     <div className='App'>
       <Movie title='Netflix Original' />
+      <Movie title='Trending Now' />
+      <Movie title='Top Rated' />
+      <Movie title='Action Movie' />
+      <Movie title='Comedy Movie' />
+      <Movie title='Horror Movie' />
+      <Movie title='Romance Movie' />
+      <Movie title='Documentaries Movie' />
     </div>
   )
 }

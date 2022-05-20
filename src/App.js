8b4ddacccc25776e73f8594a7ffaf65adf524829
nalendro16 <nalendro_agung@yahoo.com>
2022-05-20 21:34:43 +0700

    feat: fetching movie data to movie component

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ function App() {
   return (
     <div className='App'>
       <Movie title='Netflix Original' url={API.fetchNetflixOriginals} />
-      <Movie title='Trending Now' url={API.fetchTrending} />
+      {/* <Movie title='Trending Now' url={API.fetchTrending} />
       <Movie title='Top Rated' url={API.fetchTopRated} />
       <Movie title='Action Movie' url={API.fetchActionMovie} />
       <Movie title='Comedy Movie' url={API.fetchCommedyMovie} />
       <Movie title='Horror Movie' url={API.fetchHorrorMovie} />
       <Movie title='Romance Movie' url={API.fetchRomanceMovie} />
-      <Movie title='Documentaries Movie' url={API.fetchDocumentariesMovie} />
+      <Movie title='Documentaries Movie' url={API.fetchDocumentariesMovie} /> */}
     </div>
   )
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,13 +2,15 @@ import React, { useEffect } from 'react'
 import { useState } from 'react'
 import axios from '../axios'
 
+const image_url = 'https://image.tmdb.org/t/p/original'
+
 export const Movie = ({ title, url }) => {
   const [movie, setMovie] = useState([])
 
   useEffect(() => {
     const fetchData = async () => {
       const request = await axios.get(url)
-      setMovie(request.data)
+      setMovie(request?.data?.results)
     }
     fetchData()
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -19,7 +21,14 @@ export const Movie = ({ title, url }) => {
     <div>
       <h2>{title}</h2>
       <div>
-        <img src='' alt='' className='img-poster' />
+        {movie.map((item) => (
+          <img
+            src={`${image_url}${item.poster_path}`}
+            alt={`poster of ${item.name}`}
+            className='img-poster'
+            key={item.id}
+          />
+        ))}
       </div>
     </div>
   )

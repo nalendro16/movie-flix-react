ac7d07683abb6005a449bf4f1d5cc9c88e378e58
nalendro16 <nalendro_agung@yahoo.com>
2022-06-27 23:14:10 +0700

    refactor: make new component for movie card

--- src/page/Movie.jsx
+++ src/page/Movie.jsx
@@ -4,38 +4,38 @@ import { MovieList } from './MovieList'
 
 export const Movie = () => {
   return (
-    <div className='w-screen my-4'>
+    <div className='my-4'>
       <div>
         <p className='font-bold text-left ml-4 mb-4'>Trending Movie</p>
         <MovieList url={API.fetchTrending} />
       </div>
-      <div>
+      <div className='mt-4'>
         <p className='font-bold text-left ml-4 mb-4'>Action Movie</p>
         <MovieList url={API.fetchActionMovie} />
       </div>
-      <div>
+      <div className='mt-4'>
         <p className='font-bold text-left ml-4 mb-4'>Commedy Movie</p>
         <MovieList url={API.fetchCommedyMovie} />
       </div>
-      <div>
+      <div className='mt-4'>
         <p className='font-bold text-left ml-4 mb-4'>Docummentaries Movie</p>
         <MovieList url={API.fetchDocumentariesMovie} />
       </div>
-      <div>
+      <div className='mt-4'>
         <p className='font-bold text-left ml-4 mb-4'>
           Horror and Triller Movie
         </p>
         <MovieList url={API.fetchHorrorMovie} />
       </div>
-      <div>
+      <div className='mt-4'>
         <p className='font-bold text-left ml-4 mb-4'>Netflix Original Movie</p>
         <MovieList url={API.fetchNetflixOriginals} />
       </div>
-      <div>
+      <div className='mt-4'>
         <p className='font-bold text-left ml-4 mb-4'>Romance Movie</p>
         <MovieList url={API.fetchRomanceMovie} />
       </div>
-      <div>
+      <div className='mt-4'>
         <p className='font-bold text-left ml-4 mb-4'>Top Rated Movie</p>
         <MovieList url={API.fetchTopRated} />
       </div>
